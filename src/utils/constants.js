dd4495175d7822d7f022d5a59f6c295aa3569cb2
celia qiang <celia.qiang@qualitrolcorp.com>
2026-01-08 18:47:20 +0000

    use on line backend
    Signed-off-by: celia qiang <celia.qiang@qualitrolcorp.com>

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 // src/constants.js
 
-//export const API_BASE = "https://backend-all-6q0a.onrender.com/api/hsk";
-export const API_BASE = "http://localhost:5000/api/hsk";
+export const API_BASE = "https://backend-all-6q0a.onrender.com/api/hsk";
+//export const API_BASE = "http://localhost:5000/api/hsk";
 
 export const DEFAULT_QUIZ_COUNT = 20;
 export const DEFAULT_TTS_VOICE = "Mandarin Male (Yunjian)";
